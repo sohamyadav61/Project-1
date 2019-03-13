6a1ed833cff999d2a09a3979e590a7e43a575671
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-03-13 15:52:46 -0400

    created class for html

--- src/Code.php
+++ src/Code.php
@@ -7,3 +7,22 @@ class main  {
         $table = html::createTable($records);
     }
 }
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
+}
