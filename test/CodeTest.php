24e1a7eadd5587bef613c8015f24c4e6c329a0f7
soham <sy357@njit.edu>
2019-03-13 15:25:06 -0400

    Created test codes to see if the directories existed

--- test/CodeTest.php
+++ test/CodeTest.php
@@ -1,1 +1,16 @@
 <?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class CodeTest extends TestCase
+{
+    public function testSrcDirectory()
+    {
+        $this->assertDirectoryExists('src');
+    }
+    public function testTestDirectory()
+    {
+        $this->assertDirectoryExists('test');
+    }
+}
