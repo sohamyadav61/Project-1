ba57cc0692afbe480052b5e621386ecebad73cb8
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-03-13 16:25:24 -0400

    tested test directory

--- src/Code.php
+++ src/Code.php
@@ -7,3 +7,41 @@ class main  {
         $table = html::createTable($records);
     }
 }
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
+}
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
--- test/CodeTest.php
+++ test/CodeTest.php
@@ -1,1 +1,20 @@
 <?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class CodeTest extends TestCase
+{
+    public function testDataDirectory()
+    {
+        $this->assertDirectoryExists('data');
+    }
+    public function testCSVFileExists() {
+        $this->assertFileExists('data/data.csv');
+    }
+    public function testSrcDirectory(){
+        $this->assertDirectoryExists('src');
+    }
+    public function testTestDirectory(){
+        $this->assertDirectoryExists('test');
+    }
+
+}
