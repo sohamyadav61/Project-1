484255228fed9577b8c188ffc1d711bf5154294a
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-03-13 15:56:31 -0400

    tested data directory

--- test/CodeTest.php
+++ test/CodeTest.php
@@ -1,1 +1,11 @@
 <?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class CodeTest extends TestCase
+{
+    public function testDataDirectory()
+    {
+        $this->assertDirectoryExists('data');
+    }
+
+}
