c592733868a4eac5200b1a04d0b76c73f269a327
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-03-13 15:54:10 -0400

    created class for csv

--- src/Code.php
+++ src/Code.php
@@ -26,3 +26,22 @@ class html {
         }
     }
 }
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
