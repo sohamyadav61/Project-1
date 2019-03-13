2da81ca581242f02d20dc6c97b702463a903a41b
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-03-13 16:02:11 -0400

    tested test directory

--- test/CodeTest.php
+++ test/CodeTest.php
@@ -13,5 +13,8 @@ final class CodeTest extends TestCase
     public function testSrcDirectory(){
         $this->assertDirectoryExists('src');
     }
+    public function testTestDirectory(){
+        $this->assertDirectoryExists('test');
+    }
 
 }
