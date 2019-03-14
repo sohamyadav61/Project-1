4c5adc6e00cf0a09a7a960fb8ce1c54e58cc25ed
soham <sy357@njit.edu>
2019-03-13 21:18:55 -0400

    Added two new function to the code.php

--- src/Code.php
+++ src/Code.php
@@ -50,6 +50,7 @@ class record
     public function __construct(Array $fieldNames = null, $values = null)
     {
         $record = array_combine($fieldNames, $values);
+
         foreach ($record as $property => $value) {
             $this->createProperty($property, $value);
         }
@@ -63,3 +64,13 @@ class recordFactory {
     }
 }
 
+public function createProperty($name = 'first', $value = 'keith') {
+    $this->{$name} = $value;
+}
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
+    }
+}
+}
